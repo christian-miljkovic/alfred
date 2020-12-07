f3716d319552e7c6f330b82eeb79ecadebf9e769
christian-miljkovic <christian.miljkovic@gmail.com>
2020-12-07 00:06:13 -0500

    fix: not using dict for twil payload

--- alfred/api/v1/recommendations_endpoint.py
+++ alfred/api/v1/recommendations_endpoint.py
@@ -14,10 +14,11 @@ async def index(request: Request, db: DataBase = Depends(get_database)):
     logging.warning('got a recommendation')
     async with db.pool.acquire() as conn:
         try:
+            logging.warning(request)
             twilio_payload = await processors.twilio_request(request)
             existing_client = await clients.find_client_by_phone(conn, twilio_payload.user_identifier)
             if existing_client:
-                req = {"client_id": existing_client.id, "message": twilio_payload.get("current_input","")}
+                req = {"client_id": existing_client.id, "message": twilio_payload.current_input)}
                 response = await requests.post(f"{config.RECOMMENDATION_APP_URL}/recommendations", data=json.dumps(req))
                 logging.warning(response)
                 message = twilio_helper.compose_mesage(f"{constants.RECOMMENDATION_MESSAGE}")
