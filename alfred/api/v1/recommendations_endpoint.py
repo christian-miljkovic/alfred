ead54994691fdceca7d023c5458c7c7073b0d3b2
christian-miljkovic <christian.miljkovic@gmail.com>
2020-12-07 19:49:00 -0500

    fix: UUID and json serialization bug

--- alfred/api/v1/recommendations_endpoint.py
+++ alfred/api/v1/recommendations_endpoint.py
@@ -1,10 +1,10 @@
-from alfred.core import config, constants, processors
+from alfred.core import config, constants, processors, utils
 from alfred.crud import clients
 from alfred.db.database import DataBase, get_database
 from alfred.lib import TwilioHelper
 from fastapi import APIRouter, Depends, Request
 import logging
-import json
+
 import requests
 
 twilio_helper = TwilioHelper()
@@ -21,7 +21,8 @@ async def index(request: Request, db: DataBase = Depends(get_database)):
             existing_client = await clients.find_client_by_phone(conn, twilio_payload.user_identifier)
             if existing_client:
                 req = {"client_id": existing_client.id, "message": twilio_payload.current_input}
-                response = await requests.post(f"{config.RECOMMENDATION_APP_URL}/recommendations", data=json.dumps(req))
+                json_data = utils.create_aliased_response(req)
+                response = await requests.post(f"{config.RECOMMENDATION_APP_URL}/recommendations", data=json_data)
                 logging.warning(response)
                 message = twilio_helper.compose_mesage(f"{constants.RECOMMENDATION_MESSAGE}")
                 return message
