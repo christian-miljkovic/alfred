c492240c5e36c5295df7147791ffebe4244aa910
christian-miljkovic <christian.miljkovic@gmail.com>
2020-12-08 22:19:55 -0500

    fix: encoding for payload changed

--- alfred/api/v1/recommendations_endpoint.py
+++ alfred/api/v1/recommendations_endpoint.py
@@ -19,10 +19,11 @@ async def index(request: Request, db: DataBase = Depends(get_database)):
             twilio_payload = await processors.twilio_request(request)
             logging.warning(twilio_payload)
             existing_client = await clients.find_client_by_phone(conn, twilio_payload.user_identifier)
+            
             if existing_client:
-                req = {"client_id": str(existing_client.id), "message": twilio_payload.current_input}
-                response = requests.post(f"{config.RECOMMENDATION_APP_URL}/recommendations", data=json.dumps(req))
-                logging.warning(response)
+                req = {"client_id": str(existing_client.id), "message": str(twilio_payload.current_input)}
+                response = requests.post(f"{config.RECOMMENDATION_APP_URL}/recommendations", data=req)
+                logging.warning(response.content)
                 message = twilio_helper.compose_mesage(f"{constants.RECOMMENDATION_MESSAGE}")
                 return message
         except Exception as e:
