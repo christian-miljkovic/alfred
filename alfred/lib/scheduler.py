fe55278d6fec0b3ad1e4b88b185f99bd125c364a
christian-miljkovic <christian.miljkovic@gmail.com>
2021-01-10 23:08:36 -0500

    fix: formatting

--- alfred/lib/scheduler.py
+++ alfred/lib/scheduler.py
@@ -6,6 +6,7 @@ import logging
 
 def group_friends_by_client_id(friends_list: List[FriendInDB] = []) -> DefaultDict:
     friend_map = defaultdict(list)
+
     # should add some sort of unique id here
     for friend in friends_list:
         friend_map[str(friend.client_id)].append(friend.first_name + " " + friend.last_name)
